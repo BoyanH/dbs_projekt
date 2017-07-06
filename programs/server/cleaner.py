851bd801bb7a53b290474d9f4e2381b9634cd006
Boyan Hristov <bhristov96@gmail.com>
2017-07-06 17:45:31 +0200

    Deployed app to heroku

--- programs/server/cleaner.py
+++ programs/server/cleaner.py
@@ -1,11 +1,15 @@
 import csv
 from Contract import Contract
+import os
 
 def cleanData():
 	filepath = Contract.CSV_INITIAL
+	root_dir = os.path.dirname(os.getcwd())
+	explicitPathRead = os.path.join(root_dir, filepath)
+	explicitPathWrite = os.path.join(root_dir, Contract.CSV_CLEAN)
 
-	csvfile = open(filepath, "r", encoding="cp1252")
-	csv_filewrite = open(Contract.CSV_CLEAN, "w+", encoding="cp1252")
+	csvfile = open(explicitPath, "r", encoding="cp1252")
+	csv_filewrite = open(explicitPathWrite, "w+", encoding="cp1252")
 
 	csv_reader = csv.DictReader(csvfile, delimiter=";", quotechar='"')
 	csv_writer = csv.DictWriter(csv_filewrite, fieldnames=csv_reader.fieldnames,  delimiter=';', quotechar='"')
